d8b5584f270e681e484c3debb68f84a153c0d27b
MeRabia <45731228+MeRabia@users.noreply.github.com>
2020-05-23 00:57:19 +0000

    Updates

---
+++ precache-manifest.590873e895136200a2aa6224071ab355.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd8f54a057767a1d0d61",
+    "url": "/deploy-react/static/css/main.d63fdb17.chunk.css"
+  },
+  {
+    "revision": "bd8f54a057767a1d0d61",
+    "url": "/deploy-react/static/js/main.bd8f54a0.chunk.js"
+  },
+  {
+    "revision": "f4780f87ef0971ab2d04",
+    "url": "/deploy-react/static/css/1.c8e5fc35.chunk.css"
+  },
+  {
+    "revision": "f4780f87ef0971ab2d04",
+    "url": "/deploy-react/static/js/1.f4780f87.chunk.js"
+  },
+  {
+    "revision": "a72cbe425345309916a7",
+    "url": "/deploy-react/static/js/runtime~main.a72cbe42.js"
+  },
+  {
+    "revision": "b09cf516bce117648777e2ad7a7a4273",
+    "url": "/deploy-react/index.html"
+  }
+];
